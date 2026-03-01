885ca269d423934b88efc6c1c693051760644a33
Ratchanon Noknoy <ratchanon.noknoy2318@gmail.com>
2026-03-01 14:31:25 +0700

    Refactor SQL query for patient data extraction

--- SQL/extract_patient_data.sql
+++ SQL/extract_patient_data.sql
@@ -1,25 +1,26 @@
 SELECT
-person.prename AS `คํานําหน้า`,
-person.fname AS `ชื่อ`,
-person.lname AS `นามสกุล`,
-person.sex AS `เพศ`,
-right(birth,2) AS `วันเกิด`,
-mid(birth,6,2) AS `เดือนเกิด`,
-YEAR(birth) AS `ปีเกิด`,
-person.idcard AS `เลขบัตรประชาชน`,
-person.typelive AS `ประเภทที่อยู่อาศัย`,
-house.villcode AS `เลขที่อยู่อาศัย`,
-village.villname AS `ชื่อหมู่บ้าน`,
-person.hnomoi AS `บ้านเลขที่`,
-house.road AS `ชื่อถนน`,
-house.xgis AS `ละติจูด`,
-house.ygis AS `ลองติจูด`,
-'' AS `ประเภทผู้ป่วย`,
-'' AS หนังสือเดินทาง
-FROM
-person
-INNER JOIN house ON person.hcode = house.hcode
-INNER JOIN village ON house.pcucode = village.pcucode AND house.villcode = village.villcode
-WHERE
-person.typelive IN(1,3) AND
-person.dischargetype = '9'
+    p.prename        AS `คํานําหน้า`,
+    p.fname          AS `ชื่อ`,
+    p.lname          AS `นามสกุล`,
+    p.sex            AS `เพศ`,
+    RIGHT(p.birth,2) AS `วันเกิด`,
+    MID(p.birth,6,2) AS `เดือนเกิด`,
+    YEAR(p.birth)    AS `ปีเกิด`,
+    p.idcard         AS `เลขบัตรประชาชน`,
+    p.typelive       AS `ประเภทที่อยู่อาศัย`,
+    h.villcode       AS `เลขที่อยู่อาศัย`,
+    v.villname       AS `ชื่อหมู่บ้าน`,
+    p.hnomoi         AS `บ้านเลขที่`,
+    h.road           AS `ชื่อถนน`,
+    h.xgis           AS `ละติจูด`,
+    h.ygis           AS `ลองติจูด`,
+    ''               AS `ประเภทผู้ป่วย`,
+    ''               AS `หนังสือเดินทาง`
+FROM person AS p
+INNER JOIN house AS h 
+    ON p.hcode = h.hcode
+INNER JOIN village AS v 
+    ON h.pcucode = v.pcucode
+   AND h.villcode = v.villcode
+WHERE p.typelive IN (1,3)
+  AND p.dischargetype = '9';
